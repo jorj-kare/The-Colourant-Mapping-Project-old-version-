0a5139a557feda0c536aa40648c2434413115dd3
jorj-kare <jorj.kare@gmail.com>
2022-03-01 20:05:35 +0100

    Debugging

--- pigments/settings.py
+++ pigments/settings.py
@@ -14,12 +14,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 ###############################
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 ##############################
